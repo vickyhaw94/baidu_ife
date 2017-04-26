ddc925f6f64ee98a3a94ebbaa5b11e4891daf30c
wei kee haw <weikee070194@gmail.com>
2017-04-26 11:04:13 +0800

    no message

---
+++ 2015_spring/task0002/js/util.js
@@ -0,0 +1,17 @@
+/*Check whether the input is an array*/
+function isArray(arr){
+	return Array.isArray(arr);
+}
+
+/*Check whether the input is a function*/
+function isFunction(fn){
+	if(typeof fn === "function"){
+		return true;
+	}else{
+		return false;
+	}
+}
+
+/*Write a method which can clone an object return as a complete copy*/
+
+
